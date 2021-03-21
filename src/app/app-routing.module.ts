2110721712bef46b6ca18abcadb6b1f25e34ffb9
sofianezerrouki <sofianeyooung@gmail.com>
2021-03-21 22:36:56 +0100

    change

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BlogComponent } from './blog/blog.component';
 import { ContactmeComponent } from './contactme/contactme.component';
 import { DetailsComponent } from './details/details.component';
+import { HomeComponent } from './home/home.component';
+import { ProjectsComponent } from './projects/projects.component';
 
 const routes: Routes = [
   {path:"contactme",component:ContactmeComponent},
   {path:"details",component:DetailsComponent},
-  {path:"projects",component:DetailsComponent},
-  {path:"blog",component:DetailsComponent},
-  {path:"home",component:DetailsComponent}
+  {path:"projects",component:ProjectsComponent},
+  {path:"blog",component:BlogComponent},
+  {path:"home",component:HomeComponent}
 ];
 
 @NgModule({
