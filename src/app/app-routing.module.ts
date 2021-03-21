10a2fe98f1a40738c704419751e358e000f728f4
sofianezerrouki <sofianeyooung@gmail.com>
2021-03-21 22:34:17 +0100

    change

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ContactmeComponent } from './contactme/contactme.component';
+import { DetailsComponent } from './details/details.component';
 
 const routes: Routes = [
-  {path:"contactme",component:ContactmeComponent}
+  {path:"contactme",component:ContactmeComponent},
+  {path:"home",component:DetailsComponent}
 ];
 
 @NgModule({
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {  Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router) { }
 
   ngOnInit(): void {
+  }
+  sayHello(){
+    this.router.navigateByUrl('contactme');
+  }
+  aboutMe(){
+    this.router.navigateByUrl('details');
+    
+  }
+  projects(){
+    this.router.navigateByUrl('contactme');
+    
+  }
+  blog(){
+    this.router.navigateByUrl('contactme');
+
   }
 
 }
